4c95ec5e0dce0e6a6a6e99a68b264c8e2b27d9b8
Richard Wijaya <richardedric13@gmail.com>
2023-08-22 21:37:00 +0800

    html dalam php

---
+++ htmldalamphp.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML dalam PHP</title>
+</head>
+<body>
+    <?php
+        echo "<h1>Hello Richard Wijaya</h1>" ;
+        echo "<p>How are you today ?</p>" ;
+    ?>
+</body>
+</html>
