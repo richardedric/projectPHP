fa87864deb20febc8abdcbf87cd00c12777ce3ad
Richard Wijaya <richardedric13@gmail.com>
2023-08-22 22:04:49 +0800

    operator aritmatika commit

---
+++ operatoraritmatika.php
@@ -0,0 +1,19 @@
+<?php
+    $angka_1 = 6;
+    $angka_2 = 13;
+
+    $tambah = $angka_1 + $angka_2;
+    $kurang = $angka_1 - $angka_2;
+    $kali = $angka_1 * $angka_2;
+    $bagi = $angka_1 / $angka_2;
+    $modulus = $angka_1 % $angka_2;
+
+    echo $angka_1 . ' + ' . $angka_2 . ' = ' . $tambah;
+    echo '<br>';
+    echo $angka_1 . ' - ' . $angka_2 . ' = ' . $kurang;
+    echo '<br>';
+    echo $angka_1 . ' * ' . $angka_2 . ' = ' . $kali;
+    echo '<br>';
+    echo $angka_1 . ' / ' . $angka_2 . ' = ' . $bagi;
+    echo '<br>';
+    echo $angka_1 . ' % ' . $angka_2 . ' = ' . $modulus;
