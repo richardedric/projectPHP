7cfbdf5ad0ee96d50d8aa179d71f4f6887de083e
Richard Wijaya <richardedric13@gmail.com>
2023-08-22 22:36:50 +0800

    assignment commit

---
+++ assignment.php
@@ -0,0 +1,43 @@
+<?php
+    // assignment
+
+    // =
+    $samaDengan = 10;
+    $samaDengan = 5;
+    echo $samaDengan;
+    echo '<br>';
+
+    // +=
+    $plusSamaDengan = 10;
+    $plusSamaDengan += 3;
+    echo $plusSamaDengan;
+    echo '<br>';
+    
+    // -=
+    $minusSamaDengan = 10;
+    $minusSamaDengan -= 3;
+    echo $minusSamaDengan;
+    echo '<br>';
+
+    // *=
+    $kaliSamaDengan = 10;
+    $kaliSamaDengan *= 3;
+    echo $kaliSamaDengan;
+    echo '<br>';
+
+    // /=
+    $bagiSamaDengan = 10;
+    $bagiSamaDengan /= 3;
+    echo $bagiSamaDengan;
+    echo '<br>';
+
+    // %=
+    $modulusSamaDengan = 10;
+    $modulusSamaDengan %= 3;
+    echo $modulusSamaDengan;
+    echo '<br>';
+
+    // .=
+    $concatSamaDengan = 'Richard ';
+    $concatSamaDengan .= 'Wijaya';
+    echo $concatSamaDengan;
