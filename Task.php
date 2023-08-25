0ac10bb3e23c88ec3883eba6bd2aecc2e8c69eff
Richard Wijaya <richardedric13@gmail.com>
2023-08-25 22:38:48 +0800

    task php 2

--- Task.php
+++ Task.php
@@ -1,6 +1,19 @@
 <?php
-for($i=1;$i<=15;$i++){
-    if($i % 3 == 0){
-        echo "$i ";
+// for($i=1;$i<=15;$i++){
+//     if($i % 3 == 0){
+//         echo "$i ";
+//     }
+// }
+
+    // Task 2
+    for($i=1 ; $i<=12 ; $i++){
+        if($i % 5 == 0){
+            echo "Cek, ";
+        }
+        elseif($i % 3 == 0){
+            echo "Halo, ";
+        }
+        else{
+            echo "$i, ";
+        }
     }
-}
