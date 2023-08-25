4188a513577082896f5f758c7c2f19115cd2c036
Richard Wijaya <richardedric13@gmail.com>
2023-08-25 23:15:48 +0800

    task php 3

--- Task.php
+++ Task.php
@@ -6,14 +6,22 @@
 // }
 
     // Task 2
-    for($i=1 ; $i<=12 ; $i++){
-        if($i % 5 == 0){
-            echo "Cek, ";
+    // for($i=1 ; $i<=12 ; $i++){
+    //     if($i % 5 == 0){
+    //         echo "Cek, ";
+    //     }
+    //     elseif($i % 3 == 0){
+    //         echo "Halo, ";
+    //     }
+    //     else{
+    //         echo "$i, ";
+    //     }
+    // }
+
+    // Task 3
+    for($i = 0 ; $i < 6 ; $i++){
+        for($j = 0 ; $j <= $i ; $j++){
+        echo "*";
         }
-        elseif($i % 3 == 0){
-            echo "Halo, ";
-        }
-        else{
-            echo "$i, ";
-        }
+        echo "<br>";
     }
