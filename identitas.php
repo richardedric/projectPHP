5ab8e4849deea399b24ec2f8ce734cc582c08c73
Richard Wijaya <richardedric13@gmail.com>
2023-08-30 23:38:12 +0900

    identitas commit

---
+++ identitas.php
@@ -0,0 +1,20 @@
+<?php
+
+    $angka1 = 10;
+    $angka2 = 10;
+
+    if($angka1 === $angka2){
+        echo 'Benar';
+    }
+    else{
+        echo 'Salah';
+    }
+
+    echo '<br>';
+
+    if($angka1 !== $angka2){
+        echo 'Benar';
+    }
+    else{
+        echo 'Salah';
+    }
