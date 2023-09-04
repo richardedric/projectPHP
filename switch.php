e280367712f09e1e501f90bd71cbf0c10eca67c0
Richard Wijaya <richardedric13@gmail.com>
2023-09-04 22:53:05 +0900

    switch commit

---
+++ switch.php
@@ -0,0 +1,16 @@
+<?php
+    $nilai = 'A';
+
+    switch($nilai){
+        case 'A':
+            echo "Nilai A";
+            break;
+        case 'B':
+            echo "Nilai B";
+            break;
+        case 'C':
+            echo "Nilai C";
+            break;
+        default:
+        echo 'Tidak Ketiganya';
+    }
