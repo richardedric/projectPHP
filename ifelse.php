f53d9cfa5c56e871b1b8f719aecd93620e3de2b1
Richard Wijaya <richardedric13@gmail.com>
2023-09-04 22:37:22 +0900

    tenary commit

---
+++ ifelse.php
@@ -0,0 +1,9 @@
+<?php
+    $umur = 24;
+
+    if($umur < 18){
+        echo "Masih di bawah umur dek";
+    }
+    else{
+        echo "Anda legal";
+    }
---
+++ ternary.php
@@ -0,0 +1,3 @@
+<?php
+    $nilai = 30;
+    echo $nilai > 50 ? 'Betul' : 'Salah';
