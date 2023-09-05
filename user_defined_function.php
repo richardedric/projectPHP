e17b7fbccd97f53e173b550c8b63a007b6fa5ef4
Richard Wijaya <richardedric13@gmail.com>
2023-09-05 22:27:32 +0900

    function commit

---
+++ user_defined_function.php
@@ -0,0 +1,16 @@
+<?php
+    function hello(){
+        return "Hy Bro, hw r u today?";
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Function</title>
+</head>
+<body>
+    <h1><?php echo hello(); ?></h1>
+</body>
+</html>    
